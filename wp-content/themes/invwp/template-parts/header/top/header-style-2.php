c9c8ad66ad4dc07714fcb618d7a18a0e96873356
Shahbaz <shahbazuddinkha@gmail.com>
2022-04-11 12:39:40 +0530

    04-11-22

--- wp-content/themes/invwp/template-parts/header/top/header-style-2.php
+++ wp-content/themes/invwp/template-parts/header/top/header-style-2.php
@@ -2,11 +2,20 @@
   <div class="container">
     <div class="top-header">
       <div class="row">
-        <div class="col-6">
-          xyzemail@gmail.com
+        <div class="col-4">
+          Call: <a href="tel:1-844-560-7790">1-844-560-7790</a>
         </div>
-        <div class="col-6">
-          Login / Signup
+        <div class="col-4">
+          Email:<a href="mailto:service@canadianinsulin.com">service@canadianinsulin.com</a>
+        </div>
+        <div class="col-4">
+          <form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+              <div>
+                  <label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
+                  <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+                  <input type="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
+              </div>
+          </form>
         </div>
       </div>
     </div>
