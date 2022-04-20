72e2f4b23873b66fd6e50de7eafa3b2eae3e6f72
Shahbaz <shahbazuddinkha@gmail.com>
2022-04-20 13:14:15 +0530

    homepage faq collapse section in mobile

--- wp-content/themes/invwp/assets/js/main.js
+++ wp-content/themes/invwp/assets/js/main.js
@@ -1,10 +1,4 @@
 (function(){
-	
-	$(document).ready(function(){
-	    $("#toggle-btn").click(function(){
-	      $("#toggle-example").collapse('toggle'); // toggle collapse
-	    });
-	});
 
     //Login/Signup modal window - by CodyHouse.co
 	function ModalSignin( element ) {
@@ -132,3 +126,37 @@
     	}
 	};
 })();
+
+// Accordion toggle js
+	$(function() {
+		var Accordion = function(el, multiple) {
+				this.el = el || {};
+				this.multiple = multiple || false;
+
+				var links = this.el.find('.article-title');
+				links.on('click', {
+						el: this.el,
+						multiple: this.multiple
+				}, this.dropdown)
+		}
+
+		Accordion.prototype.dropdown = function(e) {
+				var $el = e.data.el;
+				$this = $(this),
+						$next = $this.next();
+
+				$next.slideToggle();
+				$this.parent().toggleClass('open');
+
+				if (!e.data.multiple) {
+						$el.find('.accordion-content').not($next).slideUp().parent().removeClass('open');
+				};
+		}
+		var accordion = new Accordion($('.accordion-container'), false);
+	});
+
+	$(document).on('click', function (event) {
+		if (!$(event.target).closest('#accordion').length) {
+			$this.parent().toggleClass('open');
+		}
+	});
--- wp-content/themes/invwp/template-parts/page/home/faq.php
+++ wp-content/themes/invwp/template-parts/page/home/faq.php
@@ -2,17 +2,17 @@
   <div class="container">
     <h3 class="section-title">Frequently Asked Questions</h3>
     <?php if (have_rows('faq_collapse')) : ?>
-    <div class="accordion " id="accordionExample">
+    <div class="accordion accordion-container" id="accordion">
       <?php $y = 0;;
         while (have_rows('faq_collapse')) : the_row();
           $y++;
           $tCount = $y; ?>
       <div class="accordion-item">
-        <h6 class="accordion-header" id="toggle-btn" data-toggle="collapse" data-target="#toggle-example" aria-expanded="true"
+        <h6 class="accordion-header article-title" id="toggle-btn" data-toggle="collapse" data-target="#toggle-example" aria-expanded="true"
           aria-controls="collapse<?php echo $tCount; ?>">
-          <span class="accordion-button"><?php the_sub_field('accordion_title'); ?></span>
+          <i></i><span class="accordion-button"><?php the_sub_field('accordion_title'); ?></span>
         </h6>
-        <div id="toggle-example" class="accordion-collapse collapse" aria-labelledby="heading<?php echo $tCount; ?>" data-bs-parent="#accordionExample">
+        <div id="toggle-example" class="accordion-collapse collapse accordion-content" aria-labelledby="heading<?php echo $tCount; ?>" data-bs-parent="#accordionExample">
           <div class="accordion-body">
             <p><?php the_sub_field('accordion_description'); ?></p>
           </div>
