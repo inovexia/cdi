51868ed1251a347881512f600f845e4754b8073e
Asif Aziz <d_asifaziz@yahoo.com>
2022-04-27 22:18:08 +0800

    Accordion

--- wp-content/themes/invwp/assets/js/main.js
+++ wp-content/themes/invwp/assets/js/main.js
@@ -6,9 +6,9 @@ $(document).ready(function(){
 document.addEventListener('click', function (e) {
     e = e || window.event;
     var target = e.target || e.srcElement;
-    e.preventDefault();
 
     if ((target.hasAttribute('data-toggle') && target.getAttribute('data-toggle') == 'modal') || (target.parentElement.hasAttribute ('data-toggle') && target.parentElement.getAttribute('data-toggle') == 'modal')) {
+        e.preventDefault();
         if (target.hasAttribute('data-target')) {
             var m_ID = target.getAttribute('data-target');
             document.getElementById(m_ID).classList.add('open');
@@ -21,6 +21,7 @@ document.addEventListener('click', function (e) {
     // Close modal window with 'data-dismiss' attribute or when the backdrop is clicked
     if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'modal') || target.classList.contains('modal') ||
 		   (target.parentElement.hasAttribute('data-dismiss') && target.parentElement.getAttribute('data-dismiss') == 'modal') || target.parentElement.classList.contains('modal')) {
+        e.preventDefault();
         var modal = document.querySelector('[class="modal open"]');
         modal.classList.remove('open');
     }
@@ -34,10 +35,6 @@ function openSidePanel() {
 function closeSidePanel() {
   document.getElementById("offcanvas-sidepanel").style.width = "0";
 }
-
-body.on ('click', function (e) {
-  alert ()
-});
 
 
 /* commented 26-4-22
