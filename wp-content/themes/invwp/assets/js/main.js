7f03f01f4312cc877189c04e640adc8331fbd02e
Asif Aziz <d_asifaziz@yahoo.com>
2022-04-27 22:17:34 +0800

    Accordion

--- wp-content/themes/invwp/assets/js/main.js
+++ wp-content/themes/invwp/assets/js/main.js
@@ -6,9 +6,9 @@ $(document).ready(function(){
 document.addEventListener('click', function (e) {
     e = e || window.event;
     var target = e.target || e.srcElement;
-    e.preventDefault();
 
     if ((target.hasAttribute('data-toggle') && target.getAttribute('data-toggle') == 'modal') || (target.parentElement.hasAttribute ('data-toggle') && target.parentElement.getAttribute('data-toggle') == 'modal')) {
+        e.preventDefault();
         if (target.hasAttribute('data-target')) {
             var m_ID = target.getAttribute('data-target');
             document.getElementById(m_ID).classList.add('open');
@@ -21,6 +21,7 @@ document.addEventListener('click', function (e) {
     // Close modal window with 'data-dismiss' attribute or when the backdrop is clicked
     if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'modal') || target.classList.contains('modal') ||
 		   (target.parentElement.hasAttribute('data-dismiss') && target.parentElement.getAttribute('data-dismiss') == 'modal') || target.parentElement.classList.contains('modal')) {
+        e.preventDefault();
         var modal = document.querySelector('[class="modal open"]');
         modal.classList.remove('open');
     }
@@ -35,61 +36,27 @@ function closeSidePanel() {
   document.getElementById("offcanvas-sidepanel").style.width = "0";
 }
 
-body.on ('click', function (e) {
-  alert ()
-});
 
 
-/* commented 26-4-22
-	$(function() {
-		var Accordion = function(el, multiple) {
-				this.el = el || {};
-				this.multiple = multiple || false;
 
-				var links = this.el.find('.article-title, .shoparticle-title');
-				links.on('click', {
-						el: this.el,
-						multiple: this.multiple
-				}, this.dropdown)
-		}
+const accordionBtns = document.querySelectorAll(".accordion .accordion-title");
 
-		Accordion.prototype.dropdown = function(e) {
-				var $el = e.data.el;
-				$this = $(this),
-						$next = $this.next();
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("is-open");
 
-				$next.slideToggle();
-				$this.parent().toggleClass('open');
+    let content = this.nextElementSibling;
+    ///console.log(content);
 
-				if (!e.data.multiple) {
-						$el.find('.accordion-content, .shopaccordion-content').not($next).slideUp().parent().removeClass('open');
-				};
-		}
-		var accordion = new Accordion($('.accordion-container, .shopaccordion-container'), false);
-	});
->>>>>>> 26b6fbb1b19c4db97b93c6ef93052a61c206c30b
-
-	$(document).on('click', function (event) {
-		if (!$(event.target).closest('#accordion, #shopaccordion').length) {
-			//$this.parent().toggleClass('open');
-		}
-<<<<<<< HEAD
-	});
-});
-
-=======
-	});*/
-
-
-//sidebar filter collapse - Side-Slide
-$('.shopfilter').click(function() {
-	$('.shopfilter-slide').animate({left: "0px"}, 200);
-	 $('.shopfilter-slide').addClass('shopfilter-m');
-});
-
-$('.shopfilterclose').click(function() {
-	$('.shopfilter-slide').animate({left: "-322px"}, 200);
-	 $('.shopfilter-slide').removeClass('shopfilter-m');
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      //console.log(content.style.maxHeight);
+    }
+  };
 });
 
 function toggle_menu () {
--- wp-content/themes/invwp/sidebar-shop.php
+++ wp-content/themes/invwp/sidebar-shop.php
@@ -16,53 +16,32 @@ if ( ! is_active_sidebar( 'sidebar-shop' ) ) {
 
 <aside class="sidebar-left widget-area">
 
-<!-- Contenedor
-<div id="filter-accordion" class="filteraccordion">
-  <div class="col-12 product-categories">
-    <h4 class="filter-link">Cat 1<i class="fa fa-chevron-down"></i></h4>
-    <ul class="filter-submenu">
-      <li><a href="#">Subcat 1</a></li>
-      <li><a href="#">Subcat 2</a></li>
-      <li><a href="#">Subcat 3</a></li>
-    </ul>
-  </div>
-</div>-->
-
-
 	<?php //dynamic_sidebar( 'sidebar-shop' ); ?>
-	<div class="ourproduct-cat shopaccordion-container filteraccordion" id="filter-accordion">
+	<div class="row ">
 		<?php
-			$cat_args = array(
-                    'orderby'    => 'name',
-                    'order'      => 'ASC',
-                    'hide_empty' => false,
-                  );
-			$product_categories = get_terms('product_cat', $cat_args);
-			//print_r($product_categories);
-			foreach ($product_categories as $key => $category) {
-			?>
-			<!--<li>
-			  <a href="<?php echo get_term_link($category); ?>"><?php echo $category->name;  ?><span>(<?php echo $category->count;  ?>)</span></a>
-			</li>-->
-			<?php
-			}
-			//$pcat_args =  array('taxonomy' => 'product_cat', 'hide_empty' => false,);
-            //$Countcats = get_terms($pcat_args);
 			$cats = get_product_categories ();
 			if (! empty ($cats)) {
 				foreach ($cats as $id => $cat) {
 					?>
-					<div class="col-12 product-categories">
-							<h4 class="mb-1 shoparticle-title1 filter-link" id="shoptoggle-btn1"><?php echo '<a href="'.$cat['link'].'">' . $cat['name'] . '</a>'; ?><i class="fa fa-chevron-down"></i></h4>
+					<div class="col-12 ourproduct-cat accordion">
+							<h4 class="mb-1 accordion-title filter-link">
+								<?php echo '<a href="'.$cat['link'].'">' . $cat['name'] . '</a>'; ?>
+							</h4>
 							<?php
 							if (! empty ($cat['sub_cats'])) {
-								echo '<ul id="shoptoggle-example1" class="list-disc pl-10 accordion-content filter-submenu">';
-								foreach ($cat['sub_cats'] as $sub_cat) {
-									echo '<li>';
-										echo '<a href="'.$sub_cat['link'].'">' . $sub_cat['name'] . ' ('.$sub_cat['count'].')</a>';
-									echo '</li>';
-								}
-								echo '</ul>';
+								?>
+								<div class="accordion-content">
+									<ul class="list-disc pl-10 --filter-submenu">
+										<?php
+										foreach ($cat['sub_cats'] as $sub_cat) {
+											echo '<li>';
+												echo '<a href="'.$sub_cat['link'].'">' . $sub_cat['name'] . ' ('.$sub_cat['count'].')</a>';
+											echo '</li>';
+										}
+										?>
+									</ul>
+								</div>
+								<?php
 							}
 							?>
 					</div>
