feeb9cfe5b7af1283c414a9d2c92a959c88d9175
Sameer Haque <50763437+rajkumar-inovage@users.noreply.github.com>
2022-05-23 21:46:08 +0530

    Header completed

--- wp-content/themes/invwp-cdi-theme/header.php
+++ wp-content/themes/invwp-cdi-theme/header.php
@@ -12,20 +12,21 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-	<?php wp_head(); ?>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
+    <?php wp_body_open(); ?>
+    <div id="page" class="site">
 
-	<header id="masthead" class="site-header">
-		<!-- Header Template Loader -->
-		<?php get_template_part('template-parts/header/header'); ?>		
-	</header><!-- #masthead -->
+        <header id="masthead" class="site-header">
+            <!-- Header Template Loader -->
+            <?php get_template_part('template-parts/header/header'); ?>
+        </header><!-- #masthead -->
