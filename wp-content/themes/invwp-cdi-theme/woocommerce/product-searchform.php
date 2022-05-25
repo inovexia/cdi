050516be224e42dc0a7837b02d7b75653316757c
Asif Aziz <asifaziz01@users.noreply.github.com>
2022-05-25 17:32:39 +0530

    Product seacrh functionality working

--- wp-content/themes/invwp-cdi-theme/woocommerce/product-searchform.php
+++ wp-content/themes/invwp-cdi-theme/woocommerce/product-searchform.php
@@ -23,6 +23,40 @@ if ( ! defined( 'ABSPATH' ) ) {
 <form role="search" method="get" class="woocommerce-product-search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label class="screen-reader-text" for="woocommerce-product-search-field-<?php echo isset( $index ) ? absint( $index ) : 0; ?>"><?php esc_html_e( 'Search for:', 'woocommerce' ); ?></label>
 	<input type="search" id="woocommerce-product-search-field-<?php echo isset( $index ) ? absint( $index ) : 0; ?>" class="search-field" placeholder="<?php echo esc_attr__( 'Search products&hellip;', 'woocommerce' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+	<?php
+	$args = array(
+   'taxonomy' => 'product_cat',
+   'name' => 'product_cat',
+   'value_field' => 'slug',
+   'class' => 'something',
+	 'show_option_all'=> 'All Categories'
+	);
+	wp_dropdown_categories( $args );
+	/*
+ 	?>
+	<select class="product_cat" name="product_cat">
+		<option value="">All Categories</option>
+		<?php
+		$cats = invwp_get_product_categories ();
+		if (! empty ($cats)) {
+			foreach ($cats as $id => $cat) {
+				?>
+				<option value="<?php echo $id; ?>" class="cat"><?php echo $cat['name']; ?></option>
+				<?php
+				if (! empty ($cat['sub_cats'])) {
+					foreach ($cat['sub_cats'] as $sub_cat) {
+						?>
+						<option value="<?php echo $sub_cat['id']; ?>" class="sub-cat"><?php echo $sub_cat['name']; ?></option>
+						<?php
+					}
+				}
+				?>
+				<?php
+			}
+		}
+		?>
+	</select>
+	<?php */ ?>
 	<button type="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'woocommerce' ); ?>"><?php echo esc_html_x( 'Search', 'submit button', 'woocommerce' ); ?></button>
 	<input type="hidden" name="post_type" value="product" />
 </form>
