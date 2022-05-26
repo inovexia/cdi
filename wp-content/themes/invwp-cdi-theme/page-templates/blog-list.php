6e33bf9d52a079e94ab5edf14501e8a1a3d9e8c5
Sameer Haque <50763437+rajkumar-inovage@users.noreply.github.com>
2022-05-26 20:38:39 +0530

    article and all article

--- wp-content/themes/invwp-cdi-theme/page-templates/blog-list.php
+++ wp-content/themes/invwp-cdi-theme/page-templates/blog-list.php
@@ -5,72 +5,18 @@ get_header();
 ?>
 
 <div id="primary" class="site-content blog-page">
-  <main id="main" class="site-main">
-    <div class="container">
-      <?php
-    $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-    // are we on page one?
-    if (1 == $paged) { ?>
-      <?php
-          $args = array(
-             'post_type' => 'post',
-             'order'     => 'ASC',
-             "posts_per_page" => 1 
-          );
-
-          $post_query = new WP_Query($args);
-
-          if($post_query->have_posts() ) {
-            while($post_query->have_posts() ) {
-             $post_query->the_post();
-             ?>
-      <div class="row first-blog-row">
-        <div class="col-6">
-    <a href="<?php the_permalink(); ?>">
-          <?php
-                     if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-                       the_post_thumbnail( 'large' );
-                     }
-                    ?>
-          </a>
-        </div>
-        <div class="col-6 first-blog-column-text">
-          <p>Publish On <strong><span><?php $category = get_the_category();
-                         $allcategory = get_the_category();
-                      foreach ($allcategory as $category) {
-                        $cat_link = get_category_link($category->cat_ID);
-                      ?>
-                <a href="<?php echo $cat_link; ?>" class="card-link"><strong><?php echo $category->cat_name;; ?></strong></a>
+    <main id="main" class="site-main">
+        <div class="container">
+            <div class="entry-header">
+                <h1><strong>Community</strong> Articles</h1>
+                <p>Disclaimer: Please note that the contents of these community articles are strictly for informational
+                    purposes and should not be considered as medical advice. These community articles are not written or
+                    reviewed for medical validity by Canadian Insulin or its staff. All views and opinions expressed by
+                    the contributing authors in these community articles are not endorsed by Canadian Insulin. Always
+                    consult a medical professional for medical advice, diagnosis, and treatment.</p>
+            </div>
+            <div class="blog-row">
                 <?php
-                      }
-                      ?></span></strong> By <strong><?php the_author(); ?></strong></p>
-          <h3 class="section-title"><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h3>
-          <div class="main-para mb-4">
-            <?php $excerpt= get_the_excerpt();
-            echo substr($excerpt, 0, 90); ?>
-
-          </div>
-      <div class="blog-btn">
-      <a href="<?php the_permalink(); ?>" class="main-btn">Read More</a>
-      </div>
-          
-        </div>
-      </div>
-      <?php
-          }
-        }
-
-        ?>
-    </div>
-    <?php
-          }
-        
-
-        ?>
-    <div class="container">
-
-      <div class="blog-row">
-        <?php
           $args = array(
              'post_type' => 'post',
              'order'     => 'ASC',
@@ -84,49 +30,45 @@ get_header();
             while($post_query->have_posts() ) {
              $post_query->the_post();
              ?>
-        <div class="col-4 blog-column">
-    <a href="<?php the_permalink(); ?>">
-          <?php
+                <div class="col-4 blog-column">
+                    <div class="blog-image">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php
                      if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
                        the_post_thumbnail( 'large' );
                      }
                     ?>
-</a>
-          <p>Publish On <strong><span><?php $category = get_the_category();
-                         $allcategory = get_the_category();
-                      foreach ($allcategory as $category) {
-                        $cat_link = get_category_link($category->cat_ID);
-                      ?>
-                <a href="<?php echo $cat_link; ?>" class="card-link"><strong><?php echo $category->cat_name;; ?></strong></a>
-                <?php
-                      }
-                      ?></span></strong> By <strong><?php the_author(); ?></strong></p>
-
-          <h3 class="sub-heading"><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h3>
-          <div class="main-para mb-4">
-            <?php $excerpt= get_the_excerpt();
+                        </a>
+                    </div>
+                    <div class="blog-content">
+                        <h3 class="sub-heading"><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h3>
+                        <p>Post Date: <strong><span><?php echo get_the_date( 'dS M Y', $post->ID ); ?></span></strong>
+                        </p>
+                        <div class="main-para mb-4">
+                            <?php $excerpt= get_the_excerpt();
             echo substr($excerpt, 0, 90); ?>
 
-          </div>
-          <div class="blog-btn">
-      <a href="<?php the_permalink(); ?>" class="main-btn">Read More</a>
-      </div>
-        </div>
-        <?php
+                        </div>
+                        <div class="blog-btn">
+                            <a href="<?php the_permalink(); ?>" class="main-btn">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <?php
             }
           }
         ?>
-      </div>
+            </div>
 
-    </div>
-    <div class="clearfix"></div>
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <nav aria-label="Page navigation example">
-            <ul class="pagination">
-              <li class="page-item">
-                <?php
+        </div>
+        <div class="clearfix"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination">
+                            <li class="page-item">
+                                <?php
 
             $big = 999999999;
             echo paginate_links( array(
@@ -140,12 +82,12 @@ get_header();
 
           
         ?>
-              </li>
-            </ul>
-          </nav>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </main>
+    </main>
 </div><!-- #primary -->
 <?php get_footer(); ?>
