ca630a84b096e2407ba7e579c8772376f2f4dcb1
unknown <you@example.com>
2022-06-11 12:27:17 +0530

    11 june - styleguide css, my account

--- wp-content/themes/invwp-cdi-theme/template-parts/page/home/product-categories.php
+++ wp-content/themes/invwp-cdi-theme/template-parts/page/home/product-categories.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
         <div class="col-12">
-            <h1>Browse medication</h1>
+            <h2>Browse medication</h2>
         </div>
     </div>
 
